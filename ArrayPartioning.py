d1c9e8580709d6d3a10f57c86a63fda43659076c
unknown <sunakshi.sharma16@gmail.com>
2019-08-27 20:37:39 -0400

    Array Partitioning with O(n) time complexity and O(n) space

---
+++ ArrayPartioning.py
@@ -0,0 +1,13 @@
+
+def arrayPartition(a):
+    p_arr = []
+    for i in a:
+        if i==0:
+            p_arr.append(i)
+    for i in a:
+        if i != 0:
+            p_arr.append(i)
+    return p_arr
+
+result = arrayPartition([2,3,3,9,0,3,8,0])
+print(result)
